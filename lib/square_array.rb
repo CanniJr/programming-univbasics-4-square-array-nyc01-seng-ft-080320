b94d541a34db49f9ec7a1f14ee028dc82fbbba8e
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-28 21:39:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_numbers = []
   count = 0 
   while count < array.length do
-    puts new_numbers.push(array[count].sqrt)
+    puts new_numbers.push(array[count]**)
     count += 1
   end
 end
